50992a89b86ff24eaf746b03c294a407daeea6b9
haohonglong <947923503@qq.com>
2019-11-25 01:48:06 +0800

    modify

--- fibonacci/main.c
+++ fibonacci/main.c
@@ -4,10 +4,11 @@
 #include "../lib.h"
 #define COMMA ","
 int run(int n);
+char* fib(int n);
 int main(int argc,char* argv[])
 {
   int n,a;
-  char str[255],ss[8];
+  char str[255],ss[4],*s_fib;
   puts("put a number");
   scanf("%d",&n);
   for(int i=1;i < n;i++){
@@ -16,9 +17,29 @@ int main(int argc,char* argv[])
     strcat(str,", ");
     printf("%d, ",a);
   }
-  printf("\n%s \n",str);
-
+  printf("\n\n%s \n",str);
+  
+  s_fib = fib(n);
+  printf("\n%s \n\n",s_fib);
   return 0;
+}
+
+char* fib(int n)
+{
+    char *str,ss[4];
+    int arr[255];
+    str = malloc(sizeof(char*));
+    
+    for(int i = 0;i < n;i++){
+        if(i < 2){
+            arr[i] = 1;
+        }else{
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        strcat(str,itoa(arr[i],ss,10));
+        strcat(str,", ");
+    }
+    return str;
 }
 
 int run(int n)
